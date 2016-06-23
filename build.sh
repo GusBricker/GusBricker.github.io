579c407429df45efb473c3afa2156c4ed7255598
Chris Lapa <chris@lapa.com.au>
2016-06-23 21:32:39 +1000

    build: allow build script to be ran from anywhere.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-LESS_DIR="less"
-CSS_DIR="css"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LESS_DIR="${DIR}/less"
+CSS_DIR="${DIR}/css"
 
 echo "[less] Building"
 lessc "${LESS_DIR}/clean-blog.less" > "${CSS_DIR}/clean-blog.css"
