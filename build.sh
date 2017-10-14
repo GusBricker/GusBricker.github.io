79bb899f184e89af57c4e0b42d1d359b1d0cc425
CircleCI Build Script <chris@lapa.com.au>
2017-10-14 13:40:43 +0000

    CircleCI: copy _site contents generated from gh-pages-ci branch

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MODE="$1"
 set -euf -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -7,6 +8,7 @@ LESS_DIR="${DIR}/less"
 CSS_DIR="${DIR}/css"
 JS_DIR="${DIR}/js"
 UGLIFYJS_BIN="${DIR}/node_modules/uglify-js/bin/uglifyjs"
+LESSC_BIN="${DIR}/node_modules/less/bin/lessc"
 
 echo "[uglifyjs] Compressing clean-blog.js"
 CLEAN_BLOG_JS="${JS_DIR}/clean-blog.js"
@@ -14,7 +16,14 @@ CLEAN_BLOG_MIN_JS="${JS_DIR}/clean-blog.min.js"
 ${UGLIFYJS_BIN} "${CLEAN_BLOG_JS}" -c -o "${CLEAN_BLOG_MIN_JS}"
 
 echo "[less] Building"
-lessc "${LESS_DIR}/clean-blog.less" > "${CSS_DIR}/clean-blog.css"
+${LESSC_BIN} "${LESS_DIR}/clean-blog.less" > "${CSS_DIR}/clean-blog.css"
 
-echo "[jekyll] Working"
-jekyll serve --watch
+if [[ "x${MODE}" == "x" ]] || [[ "x${MODE}" == "xserve" ]]
+then
+    echo "[jekyll] Serving site"
+    bundle exec jekyll serve --watch
+elif [[ "x${MODE}" == "xbuild" ]]
+then
+    echo "[jekyll] Building site"
+    bundle exec jekyll build
+fi
