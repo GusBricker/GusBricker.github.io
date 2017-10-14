f25d5b94dfb93ebff133eb39bfc97118c70f4376
Chris Lapa <chris@lapa.com.au>
2017-10-15 00:54:45 +1100

    build: ci: force checkout and mv.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- .cibuild.sh
+++ .cibuild.sh
@@ -20,13 +20,13 @@ mv _site /tmp/
 # Make sure that local master matches with remote master
 # CircleCI merges made changes to master so need to reset it
 git fetch origin master
-git checkout master
+git checkout -f master
 git reset --hard origin/master
 
 # Gets _site/* files and pushes them to master branch
 shopt -s extglob
 rm -rf !(.git|.|..)
-mv /tmp/_site/* .
+mv -f /tmp/_site/* .
 git add -A .
 git commit -m "CircleCI: copy _site contents generated from gh-pages-ci branch"
 git push origin master
